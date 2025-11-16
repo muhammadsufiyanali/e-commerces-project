484ecf156b0befce003c4e974794cce84b01867c
muhammadsufiyanali <muhammadsufiyanali34@mail.com>
2025-11-16 16:23:25 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const navbar = document.getElementById("links");
+const menubtn = document.getElementById("menu-btn");
+
+menubtn.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
+
+
